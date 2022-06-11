5b6e11aa996a238e3814ae0e75c6da274349d0ff
2006007 <2006007@iisigroup.com>
2022-06-11 18:20:26 +0800

    商品功能 - 設定返回的時間格式和時區

--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -2,6 +2,7 @@ package com.example.ecommerce.model;
 
 import com.example.ecommerce.constant.ProductCategory;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
